f9f0f76cd63e8ed9df36595c90cddec53e8b36d5
Iqra Ahmed <iqra.ahmed@uconn.edu>
2012-11-26 23:13:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IqraApp::Application.config.secret_token = '46ff752bc85bf09d3125667fd7184f87c464cabd6bf867c4f0519a2ea7f44e432d0cd728afb96da2f565ac15cbb267a1fc00d3718921e0aece4511812586051a'
